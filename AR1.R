5107bc9ddfecf54bd27170b53ce76337c2e3673e
Ruinan Zhang <zhangr4@rose-hulman.edu>
2016-12-15 00:00:05 -0500

    update ar1

--- AR1.R
+++ AR1.R
@@ -2,24 +2,41 @@
 
 # My AR(1) model
 
-ar<-arima.sim(list(ar=0.9),n=100)
+ar<-arima.sim(list(ar=0.9),n=1024)
 plot(ar)
 
 acf(ar)
+
+generate_ar <- function(initial = 0, n=1024, phi, error_sd = 1){
+  sim = c(initial)
+  for (i in 1:1024){
+    sim[i+1] = sim[i]*phi+rnorm(n=1,mean=0,sd=error_sd)
+  }
+  return(sim)
+}
  
 
 # ACF
-sim_mean <-mean(sim)
-ACF <- c()
 
-n=100
-x=ar
+sim = generate_ar(phi=0.2)
+plot(sim,type = "l")
 
-for(i in 1:20){
-  ACF[i]=(1/100)*(sum((x[i:(n)]-sim_mean)*(x[1:(n-i+1)]-sim_mean)))
+get_acf<- function(sim,max_lag=30){
+  sim_mean <- mean(sim)
+  n = length(sim)-1
+  ACF <- c()
+  for(i in 1:max_lag){
+    ACF[i]=(1/n)*(sum((sim[i:(n)]-sim_mean)*(sim[1:(n-i+1)]-sim_mean)))
+  }
+  ACF=ACF/ACF[1]
+  return(ACF)
 }
 
-ACF=ACF/ACF[1]
+acf <- get_acf(sim)
+plot(acf,type='h')+abline(h=2/sqrt(length(sim)-1),col='blue')+abline(h=-2/sqrt(length(sim)-1),col='blue')
 
-plot(ACF,type = "h")
+
+# Check for validity 
+
+
 
